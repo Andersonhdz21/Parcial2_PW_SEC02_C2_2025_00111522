8ff481da6bc6307daa2d1bca03f86ff9721bf608
Andersonhdz21 <00111522@uca.edu.sv>
2025-10-24 07:44:35 -0600

    feat:Busqueda con parametros usando querys

--- src/controladores/controladores_cuentas.js
+++ src/controladores/controladores_cuentas.js
@@ -17,3 +17,31 @@ exports.getCuentaById = (req, res) => {
     res.json({ finded: false });
   }
 };
+
+exports.getCuentaByQuery = (req, res) => {
+  const query = req.query.queryParam;
+  const isActiveFilter = req.query.isActive;
+
+  let resultados = cuentas;
+
+    if (isActiveFilter !== undefined) {
+    const isActiveBool = isActiveFilter === 'true';
+    resultados = resultados.filter(c => c.isActive === isActiveBool);
+  }
+
+  if (query) {
+    resultados = resultados.filter(c =>
+      c._id === query ||
+      c.client.toLowerCase().includes(query.toLowerCase()) ||
+      c.gender.toLowerCase() === query.toLowerCase()
+    ); 
+  }
+
+  if (resultados.length === 0) {
+    res.json({ finded: false });
+  } else if (resultados.length === 1) {
+    res.json({ finded: true, account: resultados[0] });
+  } else {
+    res.json({ finded: true, data: resultados });
+  }
+};
--- src/rutas/rutas_cuentas.js
+++ src/rutas/rutas_cuentas.js
@@ -2,6 +2,8 @@ const express = require ('express');
 const router = express.Router();
 const controladorCuentas = require ('../controladores/controladores_cuentas');
 
-router.get('/:id', controladorCuentas.getCuentaById);
+router.get('/:id', controladorCuentas.getCuentaById); //id
+
+router.get ('/', controladorCuentas.getCuentaByQuery); //querys con parametros
 
 module.exports = router;
